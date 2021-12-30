9243d08996aea044068b4506239746be89d7cc7e
徐涛 <midnite@outlook.com>
2021-12-30 10:16:30 +0800

    feat(cache):启动Redis Cache。

--- src/main/java/com/linzhi/gongfu/GongfuApplication.java
+++ src/main/java/com/linzhi/gongfu/GongfuApplication.java
@@ -2,6 +2,7 @@ package com.linzhi.gongfu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -9,6 +10,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableJpaRepositories
 @EnableWebMvc
 @EnableWebSocket
+@EnableCaching
 @SpringBootApplication
 public class GongfuApplication {
 
