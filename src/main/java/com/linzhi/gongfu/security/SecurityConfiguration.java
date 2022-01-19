5966829bf383d146873e42200bbda4b52c94d86a
徐涛 <midnite@outlook.com>
2022-01-19 13:56:03 +0800

    enhance(security):调整安全框架配置格式。

--- src/main/java/com/linzhi/gongfu/security/SecurityConfiguration.java
+++ src/main/java/com/linzhi/gongfu/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.linzhi.gongfu.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
@@ -14,6 +13,8 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * 用于在应用中启动Spring Security，并配置各个功能的入口
@@ -37,43 +38,35 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        var loginProcessingFilter = new RequestLoginProcessingFilter(successHandler, failureHandler, authenticationManagerBean());
+        var loginProcessingFilter = new RequestLoginProcessingFilter(successHandler, failureHandler,
+                authenticationManagerBean());
         var sessionProcessingFilter = new SessionLoginProcessingFilter(failureHandler, authenticationManagerBean());
         http
-            .authorizeRequests()
-            .antMatchers("/api-docs").anonymous()
-            .antMatchers("/login", "/error", "/actuator/**", "/api-docs").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .logout()
-            .logoutUrl("/logout").permitAll()
-            .logoutSuccessHandler(logoutHandler)
-            .and()
-            .exceptionHandling()
-            .authenticationEntryPoint(failureEntryPoint)
-            .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .csrf().disable()
-            .cors().disable();
+                .authorizeRequests(authorize -> authorize.antMatchers("/api-docs", "/login", "/host", "/menu",
+                        "/strings").permitAll())
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .logout(logout -> logout.logoutUrl("/logout").permitAll().logoutSuccessHandler(logoutHandler))
+                .exceptionHandling(handle -> handle.authenticationEntryPoint(failureEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable());
         http
-            .addFilterBefore(loginProcessingFilter, UsernamePasswordAuthenticationFilter.class)
-            .addFilterAfter(sessionProcessingFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(loginProcessingFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(sessionProcessingFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .authenticationProvider(sessionProvider)
-            .authenticationProvider(loginTokenProvider)
-            .authenticationProvider(sessionTokenProvider);
+                .authenticationProvider(sessionProvider)
+                .authenticationProvider(loginTokenProvider)
+                .authenticationProvider(sessionTokenProvider);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
-            .ignoring()
-            .antMatchers("/**/*.js", "/**/*.css");
+                .ignoring()
+                .antMatchers("/**/*.js", "/**/*.css");
     }
 }
