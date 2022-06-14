8ae91ddad13362f7d97b3c1bd3b6c26a2001863c
zgh <769674694@qq.com>
2022-06-14 15:34:11 +0800

    feat(cors):CORS列表里，加入 124.71.134.200:82

--- src/main/java/com/linzhi/gongfu/infrastructure/WebConfiguration.java
+++ src/main/java/com/linzhi/gongfu/infrastructure/WebConfiguration.java
@@ -14,6 +14,7 @@ public class WebConfiguration implements WebMvcConfigurer {
                     "http://localhost:9090",
                     "http://localhost:3000",
                     "http://124.71.134.200:3000",
+                    "http://124.71.134.200:82",
                     "http://192.168.2.107:3000/")
                 .allowedMethods("OPTIONS","PUT","DELETE","GET","POST","PATCH");
     }
