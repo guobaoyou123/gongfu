3b635b5d8046a5cd1055f5622d710220bab8e210
zgh <769674694@qq.com>
2022-10-24 13:18:50 +0800

    fix(salesContract):将销售合同列表中的客户合同号更改为配对后客户本单位的合同号

--- src/main/java/com/linzhi/gongfu/repository/SalesContractsRepository.java
+++ src/main/java/com/linzhi/gongfu/repository/SalesContractsRepository.java
@@ -28,15 +28,16 @@ public interface SalesContractsRepository
         " b.id,b.code,d.revision,d.order_code as orderCode,o.code as createdBy,o.name as createdByName , b.created_at as createdAt,b.state," +
         "p.id as paired," +
         "case b.state when '0' then count(distinct  t.product_id) else count(distinct v.product_id) end as category " +
-        ",d.buyer_order_code as customerContractNo ,b.buyer_comp as buyerCompCode  ,cb.chi_short  as buyerCompName," +
+        ",pv.order_code as customerContractNo ,b.buyer_comp as buyerCompCode  ,cb.chi_short  as buyerCompName," +
         "d.total_price_vat as taxedTotal,d.confirm_total_price_vat as confirmTaxedTotal from sales_contract_base  b " +
         "left join comp_operator o on o.code= b.created_by and o.dc_comp_id = b.created_by_comp " +
         "left join sales_contract_rev d on d.id = b.id and d.revision in (select max(dr.revision) from sales_contract_rev dr where dr.id = d.id) " +
         "left join sales_contract_record_rev v on v.contract_id=d.id and v.revision = d.revision " +
         "left join sales_contract_record_temp t on t.contract_id = d.id " +
         "left join comp_base cb on  b.buyer_comp = cb.code \n" +
-        "left join purchase_contract_base p on b.paired_code=p.paired_code" +
-        " where b.created_by_comp=?1 and b.created_by=?2 and b.state =?3   group by  b.id,b.code,d.revision,d.order_code,o.code,o.name, b.created_at,b.state,p.id,d.buyer_order_code,b.buyer_comp ,cb.chi_short,d.total_price_vat,d.confirm_total_price_vat) c order  by c.createdAt ,cast(RIGHT(c.code,3) as int ) desc \n", nativeQuery = true)
+        "left join purchase_contract_base p on b.paired_code=p.paired_code " +
+        "left join purchase_contract_rev pv on pv.id = p.id and pv.revision in (select max(revision) from purchase_contract_rev  where id = pv.id)\n "+
+        " where b.created_by_comp=?1 and b.created_by=?2 and b.state =?3   group by  b.id,b.code,d.revision,d.order_code,o.code,o.name, b.created_at,b.state,p.id,d.buyer_order_code,b.buyer_comp ,cb.chi_short,d.total_price_vat,d.confirm_total_price_vat,pv.order_code) c order  by c.createdAt ,cast(RIGHT(c.code,3) as int ) desc \n", nativeQuery = true)
     List<SalesContracts> listContracts(String compId, String operator, String state);
 
     /**
@@ -50,15 +51,16 @@ public interface SalesContractsRepository
         " b.id,b.code,d.revision,d.order_code as orderCode,o.code as createdBy,o.name as createdByName , b.created_at as createdAt,b.state," +
         "p.id as paired," +
         "case b.state when '0' then count(distinct  t.product_id) else count(distinct v.product_id) end as category " +
-        ",d.buyer_order_code as customerContractNo,b.buyer_comp as buyerCompCode  ,cb.chi_short as buyerCompName," +
+        ",pv.order_code as customerContractNo,b.buyer_comp as buyerCompCode  ,cb.chi_short as buyerCompName," +
         "d.total_price_vat as taxedTotal,d.confirm_total_price_vat as confirmTaxedTotal from sales_contract_base  b " +
         "left join comp_operator o on o.code= b.created_by and o.dc_comp_id = b.created_by_comp " +
         "left join sales_contract_rev d on d.id = b.id and d.revision in (select max(dr.revision) from sales_contract_rev dr where dr.id = d.id) " +
         "left join sales_contract_record_rev v on v.contract_id=d.id and v.revision = d.revision " +
         "left join comp_base cb on  b.buyer_comp = cb.code \n" +
         "left join sales_contract_record_temp t on t.contract_id = d.id " +
-        "left join purchase_contract_base p on b.paired_code=p.paired_code" +
-        " where b.created_by_comp=?1  and b.state =?2   group by  b.id,b.code,d.revision,d.order_code,o.code,o.name, b.created_at,b.state,p.id,d.buyer_order_code,b.buyer_comp ,cb.chi_short,d.total_price_vat,d.confirm_total_price_vat) c order  by c.createdAt ,cast(RIGHT(c.code,3) as int ) desc \n", nativeQuery = true)
+        "left join purchase_contract_base p on b.paired_code=p.paired_code " +
+        "left join purchase_contract_rev pv on pv.id = p.id and pv.revision in (select max(revision) from purchase_contract_rev  where id = pv.id)\n "+
+        " where b.created_by_comp=?1  and b.state =?2   group by  b.id,b.code,d.revision,d.order_code,o.code,o.name, b.created_at,b.state,p.id,d.buyer_order_code,b.buyer_comp ,cb.chi_short,d.total_price_vat,d.confirm_total_price_vat,pv.order_code) c order  by c.createdAt ,cast(RIGHT(c.code,3) as int ) desc \n", nativeQuery = true)
     List<SalesContracts> listContracts(String compId, String state);
 
 }
