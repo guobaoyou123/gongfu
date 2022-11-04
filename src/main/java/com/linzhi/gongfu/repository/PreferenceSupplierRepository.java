132900b1aa0a45d4f486e3628fe8fb8dc321e566
zgh <769674694@qq.com>
2022-11-04 10:52:52 +0800

    feat(preferenceSupplier):新增查询优选供应商列表的查询方法

--- src/main/java/com/linzhi/gongfu/repository/PreferenceSupplierRepository.java
+++ src/main/java/com/linzhi/gongfu/repository/PreferenceSupplierRepository.java
@@ -5,6 +5,10 @@ import com.linzhi.gongfu.entity.PreferenceSupplierId;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PreferenceSupplierRepository extends CrudRepository<PreferenceSupplier, PreferenceSupplierId>, QuerydslPredicateExecutor<PreferenceSupplier> {
 
+
+    List<PreferenceSupplier>  findByPreferenceSupplierId_CompCode(String companyCode);
 }
