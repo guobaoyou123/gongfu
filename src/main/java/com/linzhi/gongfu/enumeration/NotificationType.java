5d2ff9c57c7ce1122f4a3c3811638ed5290e51fe
zgh <769674694@qq.com>
2022-11-11 11:14:53 +0800

    feat(messageType):在消息类型中新增应答类型

--- src/main/java/com/linzhi/gongfu/enumeration/NotificationType.java
+++ src/main/java/com/linzhi/gongfu/enumeration/NotificationType.java
@@ -1,13 +1,13 @@
 package com.linzhi.gongfu.enumeration;
 
 /**
- * 用于表示消息通知类型
+ * 用于表示消息通知类型 0-申请采购 1-修改交易信息 2-申请结果通知  3-呼叫 4-应答
  *
  * @author zhangguanghua
  * @create_at 2022-07-20
  */
 public enum NotificationType {
-    ENROLLED_APPLY('0'), MODIFY_TRADE('1'), ENROLLED_APPLY_HISTORY('2'),INQUIRY_CALL('3')
+    ENROLLED_APPLY('0'), MODIFY_TRADE('1'), ENROLLED_APPLY_HISTORY('2'),INQUIRY_CALL('3'),INQUIRY_RESPONSE('4')
     ;
 
     private final char type;
