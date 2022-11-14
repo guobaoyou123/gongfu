024a76d7840631ccdf730652ac7cd522fb96f1ba
zgh <769674694@qq.com>
2022-11-14 09:53:07 +0800

    feat(offer):新建保存报价信息的请求体

---
+++ src/main/java/com/linzhi/gongfu/vo/VOfferRequest.java
@@ -0,0 +1,45 @@
+package com.linzhi.gongfu.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 用于接前端保存报价价格和税模式信息的请求
+ */
+@Data
+@Jacksonized
+@NoArgsConstructor
+public class VOfferRequest {
+
+    /**
+     * 税模式
+     */
+    private String taxModel;
+
+    /**
+     * 产品列表
+     */
+    private List<VProduct> products;
+
+    @Data
+    public static class VProduct {
+
+        /**
+         * 产品序号
+         */
+        private int code;
+        /**
+         * 价格
+         */
+        private BigDecimal price;
+
+        /**
+         * 是否可以报价
+         */
+        private boolean isOffered;
+    }
+}
