5c9749258aaa2e5f1fc388777ac26224d13be138
zgh <769674694@qq.com>
2022-11-14 14:51:17 +0800

    feat(offer):新增询价记录明细详情实体（包括上次报价和上次销售价）

---
+++ src/main/java/com/linzhi/gongfu/entity/InquiryRecordDetail.java
@@ -0,0 +1,88 @@
+package com.linzhi.gongfu.entity;
+
+import com.linzhi.gongfu.enumeration.Whether;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+/**
+ * 询价记录明细详情表
+ */
+@Builder
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "message_inquiry_record")
+public class InquiryRecordDetail {
+    /**
+     * 主键
+     */
+    @EmbeddedId
+    private NotificationInquiryRecordId notificationInquiryRecordId;
+
+    /**
+     * 产品id
+     */
+    @Column(name = "product_id", length = 64)
+    private String productId;
+    /**
+     * 产品编码
+     */
+    @Column(name = "product_code", length = 20)
+    private String productCode;
+    /**
+     * 描述
+     */
+    @Column(name = "product_description", length = 100)
+    private String productDescription;
+
+    /**
+     * 品牌编码
+     */
+    @Column(name = "brand_code", length = 10)
+    private String brandCode;
+    /**
+     * 品牌名称
+     */
+    @Column(name = "brand")
+    private String brand;
+    /**
+     * 计价单位
+     */
+    @Column(name = "charge_unit")
+    private String chargeUnit;
+    /**
+     * 价格
+     */
+    @Column(name = "price")
+    private BigDecimal price;
+    /**
+     * 数量
+     */
+    @Column(name = "quantity")
+    private BigDecimal amount;
+    /**
+     * 是否可以报价
+     */
+    @Column(name = "is_offer")
+    private Whether isOffer;
+
+    /**
+     * 上次报价
+     */
+    @Column
+    private BigDecimal preOfferedPrice;
+
+    /**
+     * 上次卖给该客户价格
+     */
+    @Column
+    private BigDecimal preSalesPrice;
+
+}
