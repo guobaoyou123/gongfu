c08410c63934d3009d01b98ea5cff6151916021c
zgh <769674694@qq.com>
2022-11-14 13:20:38 +0800

    feat(offer):新增查看询价记录请求的响应体

---
+++ src/main/java/com/linzhi/gongfu/vo/VOfferResponse.java
@@ -0,0 +1,118 @@
+package com.linzhi.gongfu.vo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 用于响应前端对于查看询价记录详情的请求
+ *
+ * @author zgh
+ * @create_at 2022-11-14
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Jacksonized
+@SuperBuilder(toBuilder = true)
+public class VOfferResponse extends VBaseResponse {
+    /**
+     * 消息详情
+     */
+    private VNotification offer;
+
+    @Data
+    public static class VNotification {
+
+        /**
+         * 消息通知编码
+         */
+        private String code;
+
+        /**
+         * 通知内容
+         */
+        private String content;
+
+        /**
+         * 询价单主键
+         */
+        private String inquiryId;
+
+        /**
+         * 税模式
+         */
+        private String taxModel;
+
+        /**
+         * 报价产品列表
+         */
+        private List<VProduct> products;
+    }
+
+    @Data
+    public static class VProduct{
+        /**
+         * 序号
+         */
+        private Integer itemNo;
+
+        /**
+         * 产品id
+         */
+        private String id;
+
+        /**
+         * 产品编码
+         */
+        private String code;
+
+        /**
+         * 描述
+         */
+        private String describe;
+
+        /**
+         * 计价单位
+         */
+        private String chargeUnit;
+
+        /**
+         * 品牌编码
+         */
+        private String brandCode;
+
+        /**
+         * 品牌名称
+         */
+        private String brandName;
+
+        /**
+         * 价格
+         */
+        private BigDecimal price;
+
+        /**
+         * 数量
+         */
+        private BigDecimal amount;
+
+        /**
+         * 是否报价
+         */
+        private Boolean isOffer;
+
+        /**
+         * 上次报价单价
+         */
+        private BigDecimal preOfferedPrice;
+
+        /**
+         * 上次买给该客户的单价
+         */
+        private BigDecimal preSalesPrice;
+    }
+}
