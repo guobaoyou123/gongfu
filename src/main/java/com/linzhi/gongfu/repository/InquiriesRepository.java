446e43aa4dce2868cc79eee0b476e4e639fd1a89
zgh <769674694@qq.com>
2022-11-15 16:50:12 +0800

    delete(inquiry):删除不用的方法

--- src/main/java/com/linzhi/gongfu/repository/InquiriesRepository.java
+++ src/main/java/com/linzhi/gongfu/repository/InquiriesRepository.java
@@ -2,6 +2,7 @@ package com.linzhi.gongfu.repository;
 
 
 import com.linzhi.gongfu.entity.Inquiries;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
@@ -55,23 +56,6 @@ public interface InquiriesRepository
         " where   b.created_by_comp=?1 and b.type=?2 and b.state=?3 order by b.created_at desc,cast(RIGHT(b.code,3) as int )  desc ",
         nativeQuery = true)
     List<Inquiries> listInquiries(String compId, String type, String state);
-
-
-    /**
-     * 询价单详情
-     *
-     * @param id 询价单主键
-     * @return 询价单详情
-     */
-    @Query(value = "select b.*,o.name as createdByName ,c.code as salesContractCode,r.order_code as salesOrderCode,d.order_code as orderCode from   inquiry_base b\n" +
-        "left join comp_operator o on b.created_by_comp = o.dc_comp_id and b.created_by = o.code\n" +
-        "left join sales_contract_base c on c.id = b.sales_contract_id\n" +
-        "left join sales_contract_rev r on r.id = b.sales_contract_id  and r.revision in (select max(revision) from sales_contract_rev  where id = r.id)\n" +
-        "left join sales_contract_rev d on d.id = b.contract_id  and d.revision in (select max(revision) from sales_contract_rev  where id = d.id)\n" +
-
-        " where   b.id=?1 ",
-        nativeQuery = true)
-    Optional<Inquiries> findInquiryById(String id);
 
 
 
