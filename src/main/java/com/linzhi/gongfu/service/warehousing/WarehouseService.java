fa3ea7d71a56f399fd09db4f1e50e2d6bc6049b9
zgh <769674694@qq.com>
2022-12-09 11:04:25 +0800

    feat(warehouse):新增库存管理service层

---
+++ src/main/java/com/linzhi/gongfu/service/warehousing/WarehouseService.java
@@ -0,0 +1,15 @@
+package com.linzhi.gongfu.service.warehousing;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 仓库信息及处理业务服务
+ *
+ * @author zgh
+ * @create_at 2022-12-09
+ */
+@RequiredArgsConstructor
+@Service
+public class WarehouseService {
+}
