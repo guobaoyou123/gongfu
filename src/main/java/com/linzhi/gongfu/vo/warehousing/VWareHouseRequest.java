ace077f023fe7a971d032b515f7da32aebf6341b
zgh <769674694@qq.com>
2022-12-09 16:33:06 +0800

    feat(warehouse):新增添加库房的请求体

---
+++ src/main/java/com/linzhi/gongfu/vo/warehousing/VWareHouseRequest.java
@@ -0,0 +1,49 @@
+package com.linzhi.gongfu.vo.warehousing;
+
+import com.linzhi.gongfu.vo.VBaseResponse;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 用于添加库房的请求体
+ *
+ * @author zhangguanghua
+ * @create_at 2022-12-09
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Jacksonized
+@SuperBuilder(toBuilder = true)
+public class VWareHouseRequest extends VBaseResponse {
+
+        /**
+         * 仓库名称
+         */
+        private String name;
+
+        /**
+         * 库房面积
+         */
+        private BigDecimal acreage;
+
+        /**
+         * 区域编码
+         */
+        private String  areaCode;
+
+        /**
+         * 详细地址
+         */
+        private String  address;
+
+        /**
+         * 授权操作员列表
+         */
+        private List<String> AuthorizedOperators;
+
+}
