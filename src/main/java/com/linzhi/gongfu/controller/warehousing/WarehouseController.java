500f569691d7922a248e5358c6b8d24233a28363
zgh <769674694@qq.com>
2022-12-09 10:58:36 +0800

    feat(warehouse):新增库存管理controller

---
+++ src/main/java/com/linzhi/gongfu/controller/warehousing/WarehouseController.java
@@ -0,0 +1,16 @@
+package com.linzhi.gongfu.controller.warehousing;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用于处理自有库信息
+ *
+ * @author zgh
+ * @create_at 2022-12-09
+ */
+@RequiredArgsConstructor
+@RestController
+public class WarehouseController {
+
+}
